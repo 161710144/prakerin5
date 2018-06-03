745d8f98984f99ddd049cb1c0e5b3942a5c483e5
161710144 <161710144@smkassalaambandung.sch.id>
2018-06-03 22:45:35 +0700

    relasi&3controller

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $us  = User::all();
+        return View('user.index', compact('us'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('user.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request,[
+            'name' => 'required|max:255',
+            'email' => 'required|max:255|unique',
+            'password' => 'required|min:5'
+        ]);
+
+        $us = new User;
+        $us->name = $request->name;
+        $us->email = $request->email;
+        $us->password = $request->password;
+        $us->save();
+        return redirect()->route('us.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $us = User::findOrFail($id);
+        return view('user.show',compact('us'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $us = User::findOrFail($id);
+        return view('user.edit',compact('us'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $this->validate($request,[
+            'name' => 'required|max:255',
+            'email' => 'required|max:255',
+            'password' => 'required|min:5'
+        ]);
+        $us = User::findOrFail($id);
+        $us->name = $request->name;
+        $us->email = $request->email;
+        $us->password = $request->password;
+        $us->save();
+        Session::flash("flash_notification", [
+        "level"=>"success",
+        "message"=>"Berhasil mengedit <b>$us->name</b>"
+        ]);
+        return redirect()->route('user.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $us = User::findOrFail($id);
+        $us->delete();
+        Session::flash("flash_notification", [
+        "level"=>"success",
+        "message"=>"Data Berhasil dihapus"
+        ]);
+        return redirect()->route('user.index');
+    }
+}
